2f4076432667a714e1addd9a73f8bcc0f33e0dbf
nolanling <74558808+nolanling@users.noreply.github.com>
2020-11-20 13:09:27 -0500

    Update coding_practice_2020_11_20.py

--- coding_practice_2020_11_20.py
+++ coding_practice_2020_11_20.py
@@ -1,1 +1,10 @@
+#1
+print("What is your favourite colour?)
 print(input() + "?! No why, that's my favourite colour too!")
+#2
+print("how many cans are in a pack?")
+canInPack = int(input())
+print("how many packs are there?")
+packs = int(input())
+print("the total number of cans is")
+print(canInPack*packs)
